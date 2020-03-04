fb90ca148e7d48e3e9ee32d436ef2b1df6ce2713
Johnny Lin <johnny.lin.prime@gmail.com>
2020-03-04 02:58:04 -0500

    Refractor for decoupling better

---
+++ Components/SmartSwitch.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import TitleHeader from './TitleHeader';
+
+export default class SmartSwitch extends Component {
+  render() {
+    return (
+      <View>
+        <TitleHeader title="Welcome to Johnny's smart switch! " />
+      </View>
+    );
+  }
+}
