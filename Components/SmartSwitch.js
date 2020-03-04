311fafe4b0658845cc6fd781940bd512f2b18c08
Johnny Lin <johnny.lin.prime@gmail.com>
2020-03-04 03:36:24 -0500

    Add first button for SmartSwitch.js

--- Components/SmartSwitch.js
+++ Components/SmartSwitch.js
@@ -1,13 +1,27 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
 import TitleHeader from './generic/TitleHeader';
+import ColoredButton from './generic/ColoredButton';
 
 export default class SmartSwitch extends Component {
   render() {
     return (
-      <View>
-        <TitleHeader title="Welcome to Johnny's smart switch! " />
-      </View>
+      <SafeAreaView style={styles.mainContainer}>
+        <TitleHeader
+          style={{flex: 2}}
+          title="Welcome to Johnny's smart switch! "
+        />
+        <View style={{flex: 8}}>
+          <ColoredButton style={{backgroundColor: 'yellow'}} />
+        </View>
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+});
