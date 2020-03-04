532b5f16384481b7ef6332a26e90c9722158b2ea
Johnny Lin <johnny.lin.prime@gmail.com>
2020-03-04 15:13:47 -0500

    Fix flex layout

--- Components/SmartSwitch.js
+++ Components/SmartSwitch.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {View, SafeAreaView, StyleSheet} from 'react-native';
+import {View, SafeAreaView, StyleSheet, ToastAndroid} from 'react-native';
 import TitleHeader from './generic/TitleHeader';
 import ColoredButton from './generic/ColoredButton';
 
@@ -8,12 +8,18 @@ export default class SmartSwitch extends Component {
   render() {
     return (
       <SafeAreaView style={styles.mainContainer}>
-        <TitleHeader
-          style={{flex: 2}}
-          title="Welcome to Johnny's smart switch! "
-        />
-        <View style={{flex: 8}}>
-          <ColoredButton style={{backgroundColor: 'yellow'}} />
+        <View style={{flex: 2}}>
+          <TitleHeader title="Welcome to Johnny's smart switch! " />
+        </View>
+
+        <View style={[{flex: 8}, styles.buttonsContainer]}>
+          <ColoredButton
+            style={{backgroundColor: 'yellow'}}
+            buttonText="Open Computer!"
+            onPress={() => {
+              ToastAndroid.show('onPress', ToastAndroid.SHORT);
+            }}
+          />
         </View>
       </SafeAreaView>
     );
@@ -23,5 +29,9 @@ export default class SmartSwitch extends Component {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
+    flexDirection: 'column',
+  },
+  buttonsContainer: {
+    justifyContent: 'center',
   },
 });
