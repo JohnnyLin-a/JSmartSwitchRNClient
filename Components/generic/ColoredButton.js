e1273481c5ee283bfbf2eaab1a09ef10f6d776af
Johnny Lin <johnny.lin.prime@gmail.com>
2020-03-04 23:02:57 -0500

    Add Spinner for button press and disable button

--- Components/generic/ColoredButton.js
+++ Components/generic/ColoredButton.js
@@ -1,31 +1,57 @@
 import React, {Component} from 'react';
 import {TouchableWithoutFeedback, Text, StyleSheet, View} from 'react-native';
 import {responsiveFontSize} from 'react-native-responsive-dimensions';
+import FontAwesome, {
+  SolidIcons,
+  RegularIcons,
+  BrandIcons,
+} from 'react-native-fontawesome';
+import FontAwesomeSpin from './FontAwesomeSpin';
 
 export default class ColoredButton extends Component {
   state = {
     disabled: false,
-    buttonText: '',
+    disabledComponent: null,
   };
 
   onPressButton = () => {
-    this.setState({disabled: true}, async () => {
-      const response = await this.props.onPress();
-      this.setState({disabled: false});
-    });
+    this.setState(
+      {
+        disabled: true,
+        disabledComponent: (
+          <FontAwesomeSpin
+            style={[styles.textStyle, this.props.textStyle]}
+            icon={SolidIcons.syncAlt}
+          />
+        ),
+      },
+      async () => {
+        const response = await this.props.onPress();
+        console.log('response', response);
+        if (response.data.success) {
+          this.setState({disabled: false});
+        }
+      },
+    );
   };
 
   render() {
     return (
-      <View style={[this.props.style, styles.genericTouchable]}>
+      <View
+        style={[
+          this.state.disabled ? this.props.styleDisabled : this.props.style,
+          styles.genericTouchable,
+        ]}>
         <TouchableWithoutFeedback
           disabled={this.state.disabled}
           onPress={this.onPressButton}>
-          <Text style={[styles.textStyle, this.props.textStyle]}>
-            {this.state.disabled
-              ? this.state.buttonText
-              : this.props.buttonText}
-          </Text>
+          {this.state.disabled ? (
+            this.state.disabledComponent
+          ) : (
+            <Text style={[styles.textStyle, this.props.textStyle]}>
+              {this.props.buttonText}
+            </Text>
+          )}
         </TouchableWithoutFeedback>
       </View>
     );
