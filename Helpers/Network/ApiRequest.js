04c79128738f47fe02e4e1697209b78a7fbacf86
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2020-03-06 00:55:48 -0500

    Fix promise rejection warning. Add alternative to axios get request (commented out).

--- Helpers/Network/ApiRequest.js
+++ Helpers/Network/ApiRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Config from 'react-native-config';
 
 const sendGET = URLEndpoint => {
-  axios
+  return axios
     .get(URLEndpoint)
     .then(response => {
       console.log(response);
@@ -10,9 +10,33 @@ const sendGET = URLEndpoint => {
     })
     .catch(error => {
       console.log(error);
-      return error;
+      throw error;
     });
 };
+
+/* ALTERNATIVE TO AXIOS BELOW */
+// const sendGET = URLEndpoint => {
+//   return new Promise((resolve, reject) => {
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.onreadystatechange = () => {
+//       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+//         if (xhr.status === 200) {
+//           console.log('XHR 200', xhr.response);
+//           // resolve(JSON.parse(xhr.response));
+//           // Server response sometimes missing '}' to complete the json
+//           resolve({success: true});
+//         } else {
+//           console.log('XHR ' + xhr.status, xhr.response);
+//           reject({success: false, reason: xhr.status});
+//         }
+//       }
+//     };
+
+//     xhr.open('GET', URLEndpoint);
+//     xhr.send();
+//   });
+// };
 
 export const openComputer = () => {
   return sendGET(Config.SERVER_URL + 'api/v1/openMyComputer.php');
