c78d23938b5fb8f8d17b7d74a73ed8c1d7fe5ed8
Johnny Lin <johnny.lin.prime@gmail.com>
2020-03-08 23:51:32 -0400

    Add all actions needed for SmartSwitch

--- actions/SmartSwitchStatusActions.js
+++ actions/SmartSwitchStatusActions.js
@@ -1,5 +1,17 @@
-import {UPDATE_SS_TEXT_STATUS} from '../actionTypes';
+import {
+  UPDATE_SS_REQUEST_STATUS,
+  UPDATE_SS_REQUEST_SUCCESS,
+  UPDATE_SS_MOUNT_WV,
+} from '../actionTypes';
 
-export const updateSSTextStatus = payload => {
-  return {type: UPDATE_SS_TEXT_STATUS, payload: payload};
+export const updateSSRequestStatus = payload => {
+  return {type: UPDATE_SS_REQUEST_STATUS, payload: payload};
+};
+
+export const updateSSRequestSuccess = payload => {
+  return {type: UPDATE_SS_REQUEST_SUCCESS, payload: payload};
+};
+
+export const updateSSMountWV = payload => {
+  return {type: UPDATE_SS_MOUNT_WV, payload: payload};
 };
