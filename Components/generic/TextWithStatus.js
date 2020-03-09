be671be085327e00596e3bd6c9cb1c230683fd6e
Johnny Lin <johnny.lin.prime@gmail.com>
2020-03-08 23:31:47 -0400

    Decouple code and move state away from ColoredButton. Use WebView to make Network Requests. Broken, but will add react-redux support next.

---
+++ Components/generic/TextWithStatus.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import FontAwesomeSpin from './FontAwesomeSpin';
+import FontAwesome, {SolidIcons} from 'react-native-fontawesome';
+
+export const status = {
+  IDLE: 'IDLE',
+  PROCESSING: 'PROCESSING',
+  DONE: 'DONE',
+};
+
+export default class TextWithStatus extends Component {
+  state = {
+    status: status.IDLE,
+    success: false,
+  };
+  resetToIdleTimer = () => {
+    setTimeout(() => {
+      this.setState({status: status.IDLE, success: false});
+    }, 1000);
+  };
+
+  componentDidUpdate = prevProps => {
+    if (
+      prevProps.status !== this.state.status ||
+      prevProps.success !== this.state.success
+    ) {
+      this.setState(
+        {status: this.props.status, success: this.props.success},
+        () => {
+          if (this.state.status === status.DONE) {
+            this.resetToIdleTimer();
+          }
+        },
+      );
+    }
+  };
+
+  render() {
+    return (
+      <View>
+        {this.state.status === status.IDLE && (
+          <Text style={[styles.textStyle, this.props.style]}>
+            {this.props.children}
+          </Text>
+        )}
+        {this.state.status === status.PROCESSING && (
+          <FontAwesomeSpin
+            style={[styles.textStyle, this.props.style]}
+            icon={SolidIcons.syncAlt}
+          />
+        )}
+        {this.state.status === status.DONE &&
+          (this.state.success ? (
+            <Text style={[styles.textStyle, this.props.style]}>
+              <FontAwesome icon={SolidIcons.check} />
+            </Text>
+          ) : (
+            <Text style={[styles.textStyle, this.props.style]}>
+              <FontAwesome icon={SolidIcons.times} />
+            </Text>
+          ))}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    textAlign: 'center',
+    alignSelf: 'center',
+  },
+});
